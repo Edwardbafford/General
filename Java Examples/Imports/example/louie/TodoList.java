676fff94afbeebd1212ccdab83b8695deac29b10
Louie Bafford <lbafford@usa.expertsystem.local>
2020-10-22 21:50:55 -0400

    java examples

---
+++ Java Examples/Imports/example/louie/TodoList.java
@@ -0,0 +1,25 @@
+package example.louie;
+
+import example.louie.domain.TodoItem;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoList {
+    private List<TodoItem> todoItems;
+
+    public void addTodoItem(TodoItem todoItem) {
+        if (todoItems == null) {
+            todoItems = new ArrayList<TodoItem>();
+        }
+        todoItems.add(todoItem);
+    }
+
+    public List<TodoItem> getTodoItems() {
+        return todoItems;
+    }
+
+    public void setTodoItems(List<TodoItem> todoItems) {
+        this.todoItems = todoItems;
+    }
+
+}
